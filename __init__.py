f5b25ffabebcb1e6db8099c8d9c16b2c4a37d6e5
Chrisfoz <128961713+Chrisfoz@users.noreply.github.com>
2025-04-12 19:32:28 +0930

    Add __init__.py for package structure

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from .animated_diagram import AnimatedDiagram, PulsatingEdge, create_gif_from_svg
+from .html_exporter import HtmlExporter
+
+__version__ = "0.1.0"
+__all__ = ["AnimatedDiagram", "PulsatingEdge", "HtmlExporter", "create_gif_from_svg"]
